a55b02af4ca21cbf772c673f192e31d997b285b9
james <jamesnofs@quickenloans.com>
2020-08-03 18:20:17 -0400

    lab 4.3 prime numbers
    Signed-off-by: james <jamesnofs@quickenloans.com>

---
+++ PrimeNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4._3_Prime_Numbers
+{
+    public class PrimeNumbers
+    {
+        public static bool getPrime(int number2)
+        {
+            int counter = 0;
+            for (int index = 2; index < number2; index++)
+            {
+                if (number2 % index == 0)
+                {
+                    counter = 1;
+                    break;
+                }
+            }
+            if (counter == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }        
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab4._3_Prime_Numbers
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number1 = 1;
+            int count = 0;
+            string keepGoing = "y";
+
+            while (keepGoing == "y")
+            {
+                Console.WriteLine("Which Prime number are you looking for?: ");
+                int primeChoice = int.Parse(Console.ReadLine());
+
+                while (true)
+                {
+                    number1++;
+                    if (PrimeNumbers.getPrime(number1))
+                    {
+                        count++;
+                    }
+                    if (count == primeChoice)
+                    {
+                        Console.WriteLine($"The {primeChoice}(st/th) prime number is {number1}");
+                        break;
+                    }
+                }
+                Console.WriteLine("Would you like to go again? (y/n): ");
+                keepGoing = Console.ReadLine().ToLower();
+                if (keepGoing =="n")
+                {
+                    Console.WriteLine("Thanks for playing - Bye!");
+                }
+                else
+                {
+                    keepGoing = "y";
+                }
+            }
+        }
+    }
+}
